0bf2da2460cb01a0a6e9f7273c07e24741610d32
Paula Krupa <paula.krupa@gmail.com>
2020-06-15 16:55:55 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Garden from "./Garden";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        displayedString: "This is a house."
+    }
+
+    getHandleClick = (room) => {
+        return (() => {
+            this.setState({displayedString: ["This is the " , room , "."]})
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="menu">
+                <a onClick={this.getHandleClick('kitchen')}>Kitchen</a>
+                <a onClick={this.getHandleClick('bedroom')}>Bedroom</a>
+                <a onClick={this.getHandleClick(<Garden key={'garden'}/>)}>Garden</a>
+                </div>
+                <div className="main">{this.state.displayedString}</div>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/Garden.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Garden () {
+    return (
+        <span>garden of my dreams</span>
+    )
+}
+
+export default Garden;
